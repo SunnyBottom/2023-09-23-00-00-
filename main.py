8ad933cdf95a9fa83f184a356396c316c80054b8
SunnyBottom <kristinaeme@gmail.com>
2024-08-04 22:47:11 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+name = 'Имя:'
+name_a = ' Кристинус'
+print(name + name_a)
+age = 17
+name_b = 'Старый Возраст:'
+name_ee = 'Новый Возраст:'
+print(name_b,  age)
+name = 18
+print(name_ee, name)
+name_aa = 'Студент:'
+print(name_aa, bool(name_aa))
